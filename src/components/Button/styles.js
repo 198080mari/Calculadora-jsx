d8c8c4542880736cdd2c9cf773c2f516588daa89
Mari <missionariamariros@gmail.com>
2024-06-30 09:09:28 -0300

    envio

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+padding: 20px;
+border: 4px solid #fff;
+background: #4285f4;
+color: #fff;
+font-size: 1.25rem;
+font-weight: 700;
+flex: 1;  /*alinha numeros*/
+border-radius: 10px;
+cursor: pointer;
+
+&:hover {
+    opacity: 0.6;
+}
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #fff;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #fff;
+        border: 0;
+        text-align: right;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 2rem;
+        color: #202114;
+        border: 1px solid #d2d2d2;
+        border-radius: 15px;
+        margin-bottom: 30px;
+    }
+`
